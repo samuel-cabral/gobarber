c55e3b32f6f51de16ed4c5c4ddee4cfefe90bb66
Samuel Cabral <samuelcabral.mail@gmail.com>
2020-10-07 20:19:47 -0300

    apply repository, services & SOLID principles

---
+++ src/services/CreateAppointmentService.ts
@@ -0,0 +1,43 @@
+import { startOfHour } from 'date-fns';
+
+import Appointment from '../models/Appointment';
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
+/**
+ * [x] Recebimento das informações
+ * [x] Tratativa de erros/excessões
+ * [x] Acesso ao repositório
+ */
+
+interface Request {
+  provider: string;
+  date: Date;
+}
+
+class CreateAppointmentService {
+  private appointmentsRepository: AppointmentsRepository;
+
+  constructor(appointmentsRepository: AppointmentsRepository) {
+    this.appointmentsRepository = appointmentsRepository;
+  }
+
+  public execute({ date, provider }: Request): Appointment {
+    const appointmentDate = startOfHour(date);
+
+    const findAppointmentInSameDate = this.appointmentsRepository.findByDate(
+      appointmentDate,
+    );
+
+    if (findAppointmentInSameDate) {
+      throw Error('😥 This appointment is already booked!');
+    }
+
+    const appointment = this.appointmentsRepository.create({
+      provider,
+      date: appointmentDate,
+    });
+
+    return appointment;
+  }
+}
+
+export default CreateAppointmentService;
